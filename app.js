3a01b1410ae56dab6abcfb81d78d52e4601b6dfd
SivapriyaRadhakrishnan <sivapriyamr4@gmail.com>
2024-07-31 21:34:57 +0530

    SignIn completed

--- app.js
+++ app.js
@@ -11,6 +11,40 @@ app.use(Express.json())
 app.use(Cors())
 
 Mongoose.connect("mongodb+srv://siva:6282615940@cluster0.lqoh3rx.mongodb.net/blogAppnewdb?retryWrites=true&w=majority&appName=Cluster0")
+
+//SIGNIN
+app.post("/signIn",async(req,res)=>{
+    let input=req.body
+    let result=userModel.find({email:req.body.email}).then(
+        (items)=>{
+            if(items.length>0){
+                const passwordvalidator=Bcrypt.compareSync(req.body.password,items[0].password)
+                if(passwordvalidator)
+                {
+                    Jwt.sign({email:req.body.email},"blogapp"/*token name*/,{expiresIn:"1d"},
+                        (error,token)=>{
+                            if(error){
+                                res.json({"status":"Error","errorMessage":error})
+                            }
+                            else{
+                                res.json({"status":"SUCCESS","token":token,"userId":items[0]._id})
+                            }
+                        }
+                    )
+                }
+                else{
+                    res.json({"status":"INCORRECT PASSWORD"})
+                }
+            }
+            else{
+                res.json({"status":"INVALID EMAIL ID"})
+            }
+        }
+    )
+    })
+    
+
+
 //SIGNUP
 app.post("/signUp",(req,res)=>{
     let input=req.body
